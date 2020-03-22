d9d92ce89ef8cd93fbd3ad117ceddf988f0f0006
Joachim Wegrzynek <jrrwegrzynek@gmail.com>
2020-03-22 19:34:30 +0100

    throw dices = get value

--- dice-poker/js/main.js
+++ dice-poker/js/main.js
@@ -1,6 +1,34 @@
-document.getElementById("about").addEventListener("click", function() {
-    document.getElementById("from-top").style.color="red";
-});
+let hand = [];
 
 
-console.log('it does work!');
+// throwing the dice
+
+const throwDices = (arr) => {
+
+    for ( i = 0; i < 3; i++) {
+        let dice = Math.floor(Math.random()*6) + 1;
+
+        arr.push(dice);
+        arr.sort((a, b) => b - a);
+    }
+} 
+
+throwDices(hand);
+console.log(hand);
+
+
+let toStringJoined = hand.join('');
+console.log(toStringJoined);
+
+
+
+
+
+
+
+
+
+
+// const newArr = { ... hand }
+// console.log(newArr);
+
