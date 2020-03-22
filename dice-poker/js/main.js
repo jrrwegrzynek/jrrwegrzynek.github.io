e001d67f78d250978319b2afb7036ff47cb2ac90
Joachim Wegrzynek <jrrwegrzynek@gmail.com>
2020-03-22 19:47:58 +0100

    numbers to string -> string to one number// to do update

--- dice-poker/js/main.js
+++ dice-poker/js/main.js
@@ -9,6 +9,8 @@ const throwDices = (arr) => {
         let dice = Math.floor(Math.random()*6) + 1;
 
         arr.push(dice);
+
+        // sort the array ascendingly
         arr.sort((a, b) => b - a);
     }
 } 
@@ -17,18 +19,27 @@ throwDices(hand);
 console.log(hand);
 
 
-let toStringJoined = hand.join('');
-console.log(toStringJoined);
-
-
-
-
-
-
-
-
+// TO DO:  ask the player if wishes to change any of the dices; 
 
 
 // const newArr = { ... hand }
 // console.log(newArr);
+
+
+
+
+
+// change the dices to string -> chage the string to one number
+
+let changeToString = hand.join('');
+let changeStringToNumber = parseInt(changeToString, 10); 
+
+
+console.log(changeStringToNumber);
+
+
+
+
+
+
 
