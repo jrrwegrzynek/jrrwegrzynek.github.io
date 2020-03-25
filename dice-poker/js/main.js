05ce76d07221b2575db8ede914b9317b5258b43b
Joachim Wegrzynek <jrrwegrzynek@gmail.com>
2020-03-25 22:25:42 +0100

    cleaning

--- dice-poker/js/main.js
+++ dice-poker/js/main.js
@@ -1,37 +1,29 @@
-let arrayOfHands = []
-
-
+let arrayOfHands = [];
 let activePlayer = 1;
 
+//Dices DOM
 
-
-    //score
-
-
-
-    // DICES
-
-    let diceOne = document.getElementById('dice-1');
-    let diceTwo = document.getElementById('dice-2');
-    let diceThree = document.getElementById('dice-3');
-    let diceFour = document.getElementById('dice-4');
-    let diceFive = document.getElementById('dice-5');
-    let diceSix = document.getElementById('dice-6');
+let diceOne = document.getElementById('dice-1');
+let diceTwo = document.getElementById('dice-2');
+let diceThree = document.getElementById('dice-3');
+let diceFour = document.getElementById('dice-4');
+let diceFive = document.getElementById('dice-5');
+let diceSix = document.getElementById('dice-6');
 
 // throwing the dice
 
 document.getElementById('button-draw').addEventListener('click', () =>  {
+
     let hand = [];
-    // current player
 
     const throwDices = (arr) => {
-
         for ( i = 0; i < 3; i++) {
             let dice = Math.floor(Math.random()*6) + 1;
-    
             arr.push(dice);
         }
     } 
+
+    // for future record if wishes to change any of dices
     throwDices(hand);
     console.log(hand);
 
@@ -39,12 +31,12 @@ document.getElementById('button-draw').addEventListener('click', () =>  {
     let SortTheDices = parseInt(hand.sort((a, b) => b - a).join(''), 10);
     console.log(SortTheDices);
 
+    // store the score in DOM
+    let score = document.getElementById(`player-${activePlayer}-score`).textContent = SortTheDices;
+
+
     arrayOfHands.push(SortTheDices);
     console.log(arrayOfHands);
-
-
-    let score = document.getElementById(`player-${activePlayer}-score`).textContent = SortTheDices;
-  
 
     // if (activePlayer = 1) {
 
@@ -53,16 +45,13 @@ document.getElementById('button-draw').addEventListener('click', () =>  {
             diceOne.src = `img/dice-${hand[0]}.png`;
             diceTwo.src = `img/dice-${hand[1]}.png`;
             diceThree.src = `img/dice-${hand[2]}.png`;
-            activePlayer === 2;
+            // activePlayer === 2;
         } else {
             diceFour.src = `img/dice-${hand[0]}.png`;
             diceFive.src = `img/dice-${hand[1]}.png`;
             diceSix.src = `img/dice-${hand[2]}.png`;
-            activePlayer === 1;
+            // activePlayer === 1;
         }
-
-
- 
 });
 
 
