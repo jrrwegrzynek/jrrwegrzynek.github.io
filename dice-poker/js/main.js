9f834cebe6ac88dfc0b13f8f279145e58cf0568c
Joachim Wegrzynek <jrrwegrzynek@gmail.com>
2020-03-25 18:01:28 +0100

    sort&join&change to number variable

--- dice-poker/js/main.js
+++ dice-poker/js/main.js
@@ -1,10 +1,8 @@
 let hand = [];
 
-
 // throwing the dice
 
 document.getElementById('button-draw').addEventListener('click', () =>  {
-    console.log('button was clicked');
 
     const throwDices = (arr) => {
 
@@ -12,38 +10,25 @@ document.getElementById('button-draw').addEventListener('click', () =>  {
             let dice = Math.floor(Math.random()*6) + 1;
     
             arr.push(dice);
-    
         }
     } 
     throwDices(hand);
-    
-    
-    console.log(hand);
+
+    // sort the array ascendingly & join to one string & change to number(parseInt)
+    let SortTheDices = parseInt(hand.sort((a, b) => b - a).join(''), 10);
+    console.log(SortTheDices); 
 });
 
-// after the click push the array to object with arrays
+
 
 
 
 // TO DO:  ask the player if wishes to change any of the dices; 
 
 
-const newArr = { ... hand }
-console.log(newArr);
+// const newArr = { ... hand }
+// console.log(newArr);
 
-
-
-// sort the array ascendingly
-
-let SortTheDices = hand.sort((a, b) => b - a);
-
-// change the dices to string -> chage the string to one number
-
-let changeToString = hand.join('');
-let changeStringToNumber = parseInt(changeToString, 10); 
-
-
-console.log(changeStringToNumber);
 
 
 
