51867111cd0622eecd113aa440aae45218df2816
Joachim Wegrzynek <jrrwegrzynek@gmail.com>
2020-03-28 19:20:02 +0100

    if gamePlaying

--- dice-poker/js/main.js
+++ dice-poker/js/main.js
@@ -1,5 +1,8 @@
 let arrayOfHands = [[0], [0]];
 let activePlayer = 1;
+let sumOfArrayOne = 0;
+let sumOfArrayTwo = 0;
+let gamePlaying = true;
 
 
 //Dices DOM
@@ -15,54 +18,64 @@ let diceSix = document.getElementById('dice-6');
 
 document.getElementById('button-draw').addEventListener('click', () =>  {
 
-    let hand = [];
+    if (gamePlaying) {
 
-    const throwDices = (arr) => {
-        for ( i = 0; i < 3; i++) {
-            let dice = Math.floor(Math.random()*6) + 1;
-            arr.push(dice);
+        let hand = [];
+
+        const throwDices = (arr) => {
+            for ( i = 0; i < 3; i++) {
+                let dice = Math.floor(Math.random()*6) + 1;
+                arr.push(dice);
+            }
+        } 
+
+        // for future record if wishes to change any of dices
+        throwDices(hand);
+        // console.log(hand);
+
+        // sort the array ascendingly & join to one string & change to number(parseInt)
+        let SortTheDices = parseInt(hand.sort((a, b) => b - a).join(''), 10);
+
+        // If playerOne is active push the sorted dice number to array& add new score
+        if (activePlayer === 1 ) {
+
+            diceOne.src = `img/dice-${hand[0]}.png`;
+            diceTwo.src = `img/dice-${hand[1]}.png`;
+            diceThree.src = `img/dice-${hand[2]}.png`;
+            activePlayer = 2
+            arrayOfHands[0].push(SortTheDices);
+            sumOfArrayOne = arrayOfHands[0].reduce((a,c) => {
+                return a + c
+            });
+            document.getElementById(`player-1-score`).textContent = `score: ${sumOfArrayOne}`;
+            console.log(sumOfArrayOne);
+
+        } else {
+        
+            diceFour.src = `img/dice-${hand[0]}.png`;    
+            diceFive.src = `img/dice-${hand[1]}.png`;    
+            diceSix.src = `img/dice-${hand[2]}.png`; 
+            activePlayer = 1
+            arrayOfHands[1].push(SortTheDices);
+            sumOfArrayTwo = arrayOfHands[1].reduce((a,c) => {
+                return a + c
+            });
+            document.getElementById(`player-2-score`).textContent = `score: ${sumOfArrayTwo}`;
+            console.log(sumOfArrayTwo);
+
         }
-    } 
-
-    // for future record if wishes to change any of dices
-    throwDices(hand);
-    // console.log(hand);
-
-    // sort the array ascendingly & join to one string & change to number(parseInt)
-    let SortTheDices = parseInt(hand.sort((a, b) => b - a).join(''), 10);
+        console.log(arrayOfHands);
 
 
-
-    // If playerOne is active push the sorted dice number to array& add new score
-    if (activePlayer === 1 ) {
-
-        diceOne.src = `img/dice-${hand[0]}.png`;
-        diceTwo.src = `img/dice-${hand[1]}.png`;
-        diceThree.src = `img/dice-${hand[2]}.png`;
-        activePlayer = 2
-        arrayOfHands[0].push(SortTheDices);
-        let sumOfArrayOne = arrayOfHands[0].reduce((a,c) => {
-            return a + c
-        });
-        document.getElementById(`player-1-score`).textContent = `score: ${sumOfArrayOne}`;
-        console.log(sumOfArrayOne);
-
-    } else {
-    
-        diceFour.src = `img/dice-${hand[0]}.png`;    
-        diceFive.src = `img/dice-${hand[1]}.png`;    
-        diceSix.src = `img/dice-${hand[2]}.png`; 
-        activePlayer = 1
-        arrayOfHands[1].push(SortTheDices);
-        let sumOfArrayTwo = arrayOfHands[1].reduce((a,c) => {
-            return a + c
-        });
-        document.getElementById(`player-2-score`).textContent = `score: ${sumOfArrayTwo}`;
-        console.log(sumOfArrayTwo);
+        if (sumOfArrayOne > 2000 ) {
+            console.log('player one wygrywa')
+            gamePlaying = false;
+        } else if (sumOfArrayTwo > 2000) {
+            console.log('player two wygrywa')
+            gamePlaying = false;
+        }
 
     }
-    console.log(arrayOfHands);
-
 });
 
 
