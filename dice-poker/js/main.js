d16e3c5286b12d200d2b224457f62f95713fc1b9
Joachim Wegrzynek <jrrwegrzynek@gmail.com>
2020-03-29 15:17:08 +0200

    change dice in array

--- dice-poker/js/main.js
+++ dice-poker/js/main.js
@@ -13,7 +13,6 @@ let diceThree = document.getElementById('dice-3');
 let diceFour = document.getElementById('dice-4');
 let diceFive = document.getElementById('dice-5');
 let diceSix = document.getElementById('dice-6');
-// let playerWon = document.getElementById('player_won').style.display = "none";
 
 // throwing the dice
 
@@ -32,6 +31,8 @@ document.getElementById('button-draw').addEventListener('click', () =>  {
 
         // for future record if wishes to change any of dices
         throwDices(hand);
+        console.log(hand);
+
 
         // if the dieces are the same add 500 points to the player's score
         if (hand[0] ===  hand[2] && hand[2] === hand[1]) {
@@ -41,7 +42,6 @@ document.getElementById('button-draw').addEventListener('click', () =>  {
 
                 // console.log('inne');
             }
-        console.log(hand);
 
         // sort the array ascendingly & join to one string & change to number(parseInt)
         let SortTheDices = parseInt(hand.sort((a, b) => b - a).join(''), 10);
@@ -49,16 +49,28 @@ document.getElementById('button-draw').addEventListener('click', () =>  {
         // If playerOne is active push the sorted dice number to array& add new score
         if (activePlayer === 1 ) {
 
-            diceOne.src = `img/dice-${hand[0]}.png`;
-            diceTwo.src = `img/dice-${hand[1]}.png`;
-            diceThree.src = `img/dice-${hand[2]}.png`;
+                diceOne.src = `img/dice-${hand[0]}.png`,
+                diceTwo.src = `img/dice-${hand[1]}.png`,
+                diceThree.src = `img/dice-${hand[2]}.png`
+            
+/*             diceOne.addEventListener('click', () => {
+
+                hand[0] = Math.floor(Math.random()*6) + 1;
+                diceOne.src = `img/dice-${hand[0]}.png`
+                console.log('clicked')
+                console.log(hand);
+
+            }); */
+
             activePlayer = 2
             arrayOfHands[0].push(SortTheDices);
+            console.log(arrayOfHands);
             sumOfArrayOne = arrayOfHands[0].reduce((a,c) => {
                 return a + c
             });
-            document.getElementById(`player-1-score`).textContent = `score: ${sumOfArrayOne}`;
             console.log(sumOfArrayOne);
+            document.getElementById(`player-1-score`).textContent = `score: ${sumOfArrayOne}`;
+            // console.log(sumOfArrayOne);
 
         } else {
         
@@ -71,17 +83,18 @@ document.getElementById('button-draw').addEventListener('click', () =>  {
                 return a + c
             });
             document.getElementById(`player-2-score`).textContent = `score: ${sumOfArrayTwo}`;
-            console.log(sumOfArrayTwo);
+            // console.log(sumOfArrayTwo);
 
         }
-        console.log(arrayOfHands);
 
+        // console.log(arrayOfHands);
+
+
+        // First player who gets over 10000 wins
         if (sumOfArrayOne > 10000 ) {
-            console.log('player one wygrywa')
             gamePlaying = false;
             document.getElementById('player_won').textContent = "player 1 won!";
         } else if (sumOfArrayTwo > 10000 ) {
-            console.log('player two wygrywa')
             gamePlaying = false;
             document.getElementById('player_won').textContent = "player 2 won!";
         }
@@ -92,6 +105,8 @@ document.getElementById('button-draw').addEventListener('click', () =>  {
 
 // ****** TO DO:  ask the player if wishes to change any of the dices; 
 
+
+// ****** on click "play" dissaper "main_section" -> appear "play_ground"
 
 
 
