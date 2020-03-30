c4ebbd4a0a2f7085d07dc726e32b0107de8c9dbc
Joachim Wegrzynek <jrrwegrzynek@gmail.com>
2020-03-30 21:35:22 +0200

    game logic completed'

--- dice-poker/js/main.js
+++ dice-poker/js/main.js
@@ -2,9 +2,11 @@ let arrayOfHands = [[0], [0]];
 let activePlayer = 0;
 let sumOfArray = 0;
 let gamePlaying = true;
+let diceRolled = true;
 let hand;
 
 //DOM
+
 
 let diceOne = document.getElementById('dice-1');
 let diceTwo = document.getElementById('dice-2');
@@ -12,7 +14,8 @@ let diceThree = document.getElementById('dice-3');
 let diceFour = document.getElementById('dice-4');
 let diceFive = document.getElementById('dice-5');
 let diceSix = document.getElementById('dice-6');
-
+let clickedDice = document.getElementsByClassName('dice_clicked');
+let dices = [diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix];
     //BUTTONS 
     
     let buttonDraw = document.getElementById('button-draw');
@@ -40,58 +43,77 @@ buttonDraw.addEventListener('click', () =>  {
         // If playerOne is active push the sorted dice number to array& add new score
         if (activePlayer === 0 ) {
             activePlayer = 1
+            diceRolled = true;
             diceOne.src = `img/dice-${hand[0]}.png`;
             diceTwo.src = `img/dice-${hand[1]}.png`;
             diceThree.src = `img/dice-${hand[2]}.png`;
 
-            diceOne.addEventListener('click', () => {
 
-                hand[0] = Math.floor(Math.random()*6) + 1;
-                diceOne.src = `img/dice-${hand[0]}.png`
-                diceOne.disabled = true;
-                console.log(hand);
-
-            });
-        
-            diceTwo.addEventListener('click', () => {
-    
-                hand[1] = Math.floor(Math.random()*6) + 1;
-                diceTwo.src = `img/dice-${hand[1]}.png`
-                console.log(hand);
-            });
-        
-            diceThree.addEventListener('click', () => {
-        
-                hand[2] = Math.floor(Math.random()*6) + 1;
-                diceThree.src = `img/dice-${hand[2]}.png`
-                console.log(hand);
-            });
+                diceOne.addEventListener('click', () => {
+                    if (diceRolled) {
+                    hand[0] = Math.floor(Math.random()*6) + 1;
+                    diceOne.src = `img/dice-${hand[0]}.png`
+                    console.log(hand);
+                    diceOne.classList.add('dice_clicked');
+                    }
+                    diceRolled = false;
+                });
+            
+                diceTwo.addEventListener('click', () => {
+                    if (diceRolled) {
+                    hand[1] = Math.floor(Math.random()*6) + 1;
+                    diceTwo.src = `img/dice-${hand[1]}.png`
+                    console.log(hand);
+                    diceTwo.classList.add('dice_clicked');
+                    }
+                    diceRolled = false;
+                });
+            
+                diceThree.addEventListener('click', () => {
+                    if (diceRolled) {
+                    hand[2] = Math.floor(Math.random()*6) + 1;
+                    diceThree.src = `img/dice-${hand[2]}.png`
+                    console.log(hand);
+                    diceThree.classList.add('dice_clicked');
+                    }
+                    diceRolled = false;
+                });
 
         } else {
             activePlayer = 0;
+            diceRolled = true;
             diceFour.src = `img/dice-${hand[0]}.png`;    
             diceFive.src = `img/dice-${hand[1]}.png`;    
             diceSix.src = `img/dice-${hand[2]}.png`; 
 
             diceFour.addEventListener('click', () => {
-
+                if (diceRolled) {
                 hand[0] = Math.floor(Math.random()*6) + 1;
                 diceFour.src = `img/dice-${hand[0]}.png`
                 console.log(hand);
+                diceFour.classList.add('dice_clicked');
+                }
+                diceRolled = false;
             });
     
             diceFive.addEventListener('click', () => {
-    
+                if (diceRolled) {
                 hand[1] = Math.floor(Math.random()*6) + 1;
                 diceFive.src = `img/dice-${hand[1]}.png`
                 console.log(hand);
+                diceFive.classList.add('dice_clicked');
+                }
+                diceRolled = false;
             });
     
             diceSix.addEventListener('click', () => {
-    
+                if (diceRolled) {
                 hand[2] = Math.floor(Math.random()*6) + 1;
                 diceSix.src = `img/dice-${hand[2]}.png`
                 console.log(hand);
+                diceSix.classList.add('dice_clicked');
+                }
+                diceRolled = false;
             });
         }        
     }
@@ -101,6 +123,11 @@ buttonDraw.addEventListener('click', () =>  {
 document.getElementById('button-submit').addEventListener('click', () => {
 
     if (gamePlaying) {
+
+        for (i =0; i < dices.length; i++ ) {
+            dices[i].classList.remove('dice_clicked');
+        }
+
         buttonDraw.disabled = false;
         // sort the array ascendingly & join to one string & change to number(parseInt)
         let sortTheDices = parseInt(hand.sort((a, b) => b - a).join(''), 10);
@@ -127,10 +154,10 @@ document.getElementById('button-submit').addEventListener('click', () => {
             console.log(hand);
 
         // First player who gets over 10000 wins
-        if (sumOfArray > 10000 && activePlayer === 0 ) {
+        if (sumOfArray > 2000 && activePlayer === 0 ) {
             gamePlaying = false;
             document.getElementById('player_won').textContent =`player 2 won!`;
-        } else if (sumOfArray > 10000 && activePlayer === 1 ) {
+        } else if (sumOfArray > 2000 && activePlayer === 1 ) {
             gamePlaying = false;
             document.getElementById('player_won').textContent =`player 1 won!`;
         }
