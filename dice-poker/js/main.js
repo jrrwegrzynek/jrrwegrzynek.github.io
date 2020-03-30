e6689260c0a951cca0b43f64f5eda3a589b93bd3
Joachim Wegrzynek <jrrwegrzynek@gmail.com>
2020-03-30 21:56:51 +0200

    game logic opt

--- dice-poker/js/main.js
+++ dice-poker/js/main.js
@@ -3,8 +3,7 @@ let activePlayer = 0;
 let sumOfArray = 0;
 let gamePlaying = true;
 let diceRolled = true;
-let hand;
-
+let hand; 
 //DOM
 
 
@@ -16,6 +15,7 @@ let diceFive = document.getElementById('dice-5');
 let diceSix = document.getElementById('dice-6');
 let clickedDice = document.getElementsByClassName('dice_clicked');
 let dices = [diceOne, diceTwo, diceThree, diceFour, diceFive, diceSix];
+
     //BUTTONS 
     
     let buttonDraw = document.getElementById('button-draw');
@@ -31,6 +31,7 @@ buttonDraw.addEventListener('click', () =>  {
     if (gamePlaying) {
         buttonSubmit.disabled = false;
 
+        //trow the dices and store them in variable hand
         const throwDices = (arr) => {
             for ( i = 0; i < 3; i++) {
                 let dice = Math.floor(Math.random()*6) + 1;
@@ -48,12 +49,14 @@ buttonDraw.addEventListener('click', () =>  {
             diceTwo.src = `img/dice-${hand[1]}.png`;
             diceThree.src = `img/dice-${hand[2]}.png`;
 
-
+                //redraw the dice
                 diceOne.addEventListener('click', () => {
+                    //you can redraw only ine dice
                     if (diceRolled) {
                     hand[0] = Math.floor(Math.random()*6) + 1;
                     diceOne.src = `img/dice-${hand[0]}.png`
                     console.log(hand);
+                    //dice was redrawn
                     diceOne.classList.add('dice_clicked');
                     }
                     diceRolled = false;
@@ -124,6 +127,7 @@ document.getElementById('button-submit').addEventListener('click', () => {
 
     if (gamePlaying) {
 
+        //remove DOM's "clicked dice" class
         for (i =0; i < dices.length; i++ ) {
             dices[i].classList.remove('dice_clicked');
         }
@@ -142,29 +146,37 @@ document.getElementById('button-submit').addEventListener('click', () => {
     
                     // console.log('inne');
                 }               
-
+            // add the numbers of a each array
             sumOfArray = arrayOfHands[activePlayer].reduce((a,c) => {
                 return a + c
             });  
 
             console.log(sumOfArray);
+            //store the score
             document.getElementById(`player-${activePlayer}-score`).textContent = sumOfArray;
             console.log(arrayOfHands);
             console.log(sortTheDices);
             console.log(hand);
 
         // First player who gets over 10000 wins
-        if (sumOfArray > 2000 && activePlayer === 0 ) {
+        if (sumOfArray > 10000 && activePlayer === 0 ) {
             gamePlaying = false;
-            document.getElementById('player_won').textContent =`player 2 won!`;
-        } else if (sumOfArray > 2000 && activePlayer === 1 ) {
+            alert ('player 2 won!');
+            newGame();
+        } else if (sumOfArray > 10000 && activePlayer === 1 ) {
             gamePlaying = false;
-            document.getElementById('player_won').textContent =`player 1 won!`;
+            alert ('player 1 won!');
+            newGame();
         }
     }
     buttonSubmit.disabled = true;
 }); 
 
+
+// init new game
+function newGame(){
+    window.location.reload();
+} 
 
 // ****** on click "play" dissaper "main_section" -> appear "play_ground"
 
